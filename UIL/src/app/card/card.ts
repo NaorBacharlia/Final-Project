e7b3eadc448b212d86ecf477d6212ddfaa64a751
Naor bacharlia <44392584+NaorBacharlia@users.noreply.github.com>
2020-01-01 21:01:35 +0200

    final project

---
+++ UIL/src/app/card/card.ts
@@ -0,0 +1,10 @@
+
+export class Card  {
+    public cardpic:string;
+  
+    constructor(_cardpic:string){
+        this.cardpic="../../assets/pictures/all cards/"+_cardpic+".png";
+    }
+   
+       
+}
---
+++ UIL/src/app/shared/models/CardMap.ts
@@ -0,0 +1,12 @@
+export enum CardMap{
+    DeckOfCards=1,
+    UsedCards=2,
+    FirstPlayerFrontRight=3,
+    FirstPlayerFrontLeft=4,
+    FirstPlayerBackRight=5,
+    FirstPlayerBackLeft=6,
+    SecondPlayerFrontRight=7,
+    SecondPlayerFrontLeft=8,
+    SecondPlayerBackRight=9,
+    SecondPlayerBackLeft=10
+  }
