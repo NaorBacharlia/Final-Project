294e88b977ff0cedbd16ae14b518a3597ad8fa07
Ariel Gordon <arielgordon123@gmai.com>
2020-01-02 10:26:46 +0200

    Remove node_modules

--- UIL/src/app/game/game.component.ts
+++ UIL/src/app/game/game.component.ts
@@ -1,31 +1,42 @@
-import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component,OnInit, NgModule,Input,ComponentFactory,ComponentRef, ComponentFactoryResolver, ViewContainerRef, ChangeDetectorRef, TemplateRef, ViewChild, Output, EventEmitter} from '@angular/core';
 import { GameService } from '../shared/services/game.service';
 import { GeneralGameInfoModel } from '../shared/models/generalgamegame-info.model';
 import { GameOnRunModel } from '../shared/models/gameonruninfo.model';
 import {CardComponent} from '../card/card.component'; 
 import { CardValue } from '../shared/models/CardValue';
-import { from } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 import {Card} from '../card/card';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'] 
 })
 
 
 export class GameComponent implements OnInit {
+ 
+  @ViewChild("cardsContainer", { read: ViewContainerRef }) container;
+  componentRef: ComponentRef<CardComponent>;
+ 
   @ViewChild(Card) FirstPlayerFrontRight:Card;
   @ViewChild(Card) FirstPlayerFrontLeft: Card;
   @ViewChild(Card) FirstPlayerBackRight: Card;
   @ViewChild(Card) FirstPlayerBackLeft:  Card;
+
+  FirstPlayerFrontRightEvent: Subject<void> = new Subject<void>();
+  FirstPlayerFrontLeftEvent: Subject<void> = new Subject<void>();
+  FirstPlayerBackRightEvent: Subject<void> = new Subject<void>();
+  FirstPlayerBackLeftEvent: Subject<void> = new Subject<void>();
  
   public myGame:GeneralGameInfoModel;
   public gameRun:GameOnRunModel;
   // public cardvalue:CardValue;
-  constructor(private gameservice:GameService) { }
+  constructor(private gameservice:GameService,private resolver: ComponentFactoryResolver) { }
   status:boolean=false;
   state:string='card-back';
-  
+  emitEventToChild(subject:Subject<void>) {
+    subject.next();
+  }
   ngOnInit() {
     this.gameservice.startnewgame().subscribe(
       (res)=>{
@@ -35,16 +46,30 @@ export class GameComponent implements OnInit {
         console.log(this.myGame.cardTable.JackOfClover);
        
         this.setLocationCards();
-        
+        this.show2card();
       },(err)=>{console.log(err)}
       
     );
   }
+  ngOnDestroy() {
+    this.componentRef.destroy();    
+  }
+  createCardComponent(card:Card, event:Observable<void>) {
+    // this.container.clear();
+    const factory: ComponentFactory<CardComponent> = this.resolver.resolveComponentFactory(CardComponent);
+
+    this.componentRef = this.container.createComponent(factory);
+    
+    this.componentRef.instance.card = card;
+    this.componentRef.instance.events = event;
+    // this.componentRef.instance.output.subscribe(event => console.log(event));
+
+  }
 
   setLocationCards(){
-    console.log("card valu",CardValue);
+    console.log("card value",CardValue);
     // this.gameRun.GameId=this.myGame.cardTable.GameId;
-    console.log("aaa",this.myGame.cardTable)
+    console.log("cardTable",this.myGame.cardTable)
     let arrayNames=[];
     for(let value=3;value<=10; value++){
       let keyArr = Object.keys(this.myGame.cardTable);
@@ -58,7 +83,6 @@ export class GameComponent implements OnInit {
       }
       console.log(key,value);
     }
-    console.log(CardValue[arrayNames[0]])
     this.gameRun= {
     GameId: this.myGame.cardTable.GameId,
     FirstPlayerFrontRight: <number><any>CardValue[arrayNames[0]],
@@ -75,12 +99,25 @@ export class GameComponent implements OnInit {
     this.FirstPlayerFrontLeft =new Card(CardValue[arrayNames[1]]);
     this.FirstPlayerBackRight =new Card(CardValue[arrayNames[2]]);
     this.FirstPlayerBackLeft  =new Card(CardValue[arrayNames[3]]);
+    this.createCardComponent(this.FirstPlayerBackLeft, this.FirstPlayerBackLeftEvent );
+    this.createCardComponent(this.FirstPlayerBackRight, this.FirstPlayerBackRightEvent);
+   
+    
+    this.createCardComponent(this.FirstPlayerFrontLeft, this.FirstPlayerFrontLeftEvent);
+    this.createCardComponent(this.FirstPlayerFrontRight, this.FirstPlayerFrontRightEvent);
+    
     console.log(this.gameRun)
   }
 
   show2card(){
+    setTimeout(() => {
+      this.emitEventToChild(this.FirstPlayerFrontLeftEvent);
+    this.emitEventToChild(this.FirstPlayerFrontRightEvent);
+    }, 0);
     setTimeout(()=>{
-
+      console.log('changed :');
+      this.emitEventToChild(this.FirstPlayerFrontLeftEvent);
+      this.emitEventToChild(this.FirstPlayerFrontRightEvent);
     },5000)
   }
 
